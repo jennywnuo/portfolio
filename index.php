24345678a7426e62d4015b8b11f1cc6a91603ce6
jennywnuo <jennywnuo@gmail.com>
2019-11-19 17:20:37 +0000

    my first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Jenny Wang CSP Period 2]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <li><a href="Wang_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Wang_Projects.html">Projects.html</a></li></br>
+      <li><a href="Wang_Resume.html">Resume.html</a></li></br>
+      <li><a href="Wang_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
